0f366acee037fb2705dd883e68d13049fc321203
Matthew Crooks <mfcrooks@gmail.com>
2019-02-08 13:17:42 -0800

    Hijack the DOM and link up button

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,7 +1,20 @@
-// import $ from "jquery";
+// $(document).ready(function () {
+//   $("button.js_index_games").on("click",function(e){
+//     alert("You clicked this link!")
+//     e.preventDefault();
+//   })
+// });
+
 $(document).ready(function () {
-  $("button.js_index_games").on("click",function(e){
-    alert("You clicked this link!")
-    e.preventDefault();
-  })
+	console.log('game.js loaded ...');
+	getGames()
+
 });
+
+function getGames() {
+  $("button#js_index_games").on("click", function (e) {
+    e.preventDefault();
+
+    alert("You clicked this button!");
+  })
+}
